79983a36aae7f2353c070171b60cb9387b44f65e
Bharat Sen <bhrat@amwebtech.com>
2023-11-08 11:34:29 +0530

    scrap locations list

--- scrape.js
+++ scrape.js
@@ -80,24 +80,112 @@ async function getLocalPlacesInfo(requestParams) {
 }
 
 var fs = require('fs');
+var keywordList = ['hall', 'sports hall']
+// asyncS.forEachSeries(keywordList, (item, callback) => {
+//   console.log("item=======", item);
+//   const requestParams = {
+//     baseURL: `http://google.com`,
+//     query: item + ' 452011', // what we want to search
+//     hl: "en", // parameter defines the language to use for the Google maps search
+//   };
+//   let resp = getLocalPlacesInfo(requestParams).then((locations) => {
+//     console.log("locations=========", locations, locations.length)
+//     if (locations.length) {
+//       fs.writeFile(item + '.json', JSON.stringify(locations), 'utf8', (err, data) => {
+//         callback();
+//       });
+//     } else {
+//       callback();
+//     }
+//   });
+// }, () => {
+//   console.log('Scraping completed.');
+// });
 
-asyncS.forEachSeries(['hall', 'sports hall'], (item, callback) => {
-  console.log("item=======", item);
-  const requestParams = {
-    baseURL: `http://google.com`,
-    query: item + ' 452011', // what we want to search
-    hl: "en", // parameter defines the language to use for the Google maps search
-  };
-  let resp = getLocalPlacesInfo(requestParams).then((locations) => {
-    console.log("locations=========", locations, locations.length)
-    if (locations.length) {
-      fs.writeFile(item + '.json', JSON.stringify(locations), 'utf8', (err, data) => {
-        callback();
-      });
-    } else {
-      callback();
-    }
+
+
+
+
+async function fillPage(page) {
+  const dataFromPage = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".Nv2PK")).map((el) => {
+      const locationName = el.querySelector(".qBF1Pd")?.textContent.trim();
+      const addressElement = el.querySelector(".W4Efsd:last-child > .W4Efsd:nth-of-type(1) > span:last-child");
+      const address = addressElement ? addressElement.textContent.replaceAll("·", "").trim() : "";
+
+      // Additional information
+      const viewL = el.querySelector(".hfpxzc")?.getAttribute("href");
+      const keyword = el.querySelector(".qBF1Pd + span")?.textContent.trim();
+      const postCode = el.querySelector(".cfS6ac")?.textContent.trim();
+      const tel = el.querySelector(".dLRLre")?.textContent.trim();
+      const googleMapsLink = el.querySelector("a[data-value]")?.getAttribute("href");
+      const website = el.querySelector("a[data-value]")?.getAttribute("data-url");
+      const email = el.querySelector('a[href^="mailto:"]')?.textContent;
+      console.log("viewL", viewL);
+      return {
+        locationName,
+        viewL,
+        keyword,
+        postCode,
+        address,
+        tel,
+        googleMapsLink,
+        website,
+        email,
+      };
+    });
   });
-}, () => {
-  console.log('Scraping completed.');
-});
+  return dataFromPage;
+}
+
+async function PlacesInfo(requestParams) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+
+  const page = await browser.newPage();
+
+  const URL = `${requestParams.baseURL}/maps/search/${requestParams.query}?hl=${requestParams.hl}`;
+
+  await page.setDefaultNavigationTimeout(60000);
+  await page.goto(URL);
+
+  await page.waitForNavigation();
+
+  const scrollContainer = ".m6QErb[aria-label]";
+  const localPlacesInfo = [];
+
+  await page.waitForTimeout(2000);
+  await scrollPage(page, scrollContainer);
+  page.on('console', (msg) => {
+    console.log('Page Log:', msg.text());
+  });
+  localPlacesInfo.push(...(await fillDataFromPage(page)));
+
+  await browser.close();
+
+  return localPlacesInfo;
+}
+
+async function getLocationDetails () {
+  asyncS.forEachSeries(keywordList, (item, callback) => {
+    console.log("item===========", item);
+    fs.readFile(item + '.json', 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+          callback()
+      } else {
+        obj = JSON.parse(data); //now it an object
+        console.log("obj===========", obj);
+        callback()
+      }
+    });
+  }, () => {
+    console.log('Scraping completed.');
+    // getLocationDetails
+  });
+}
+
+getLocationDetails();
+
